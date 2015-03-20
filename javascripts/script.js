01782bdabfc457b4b024b1871edd1e4302d3e2c8
Mathias Peter <mathiaspeter@Mathiass-iMac.local>
2015-03-20 10:03:06 +0100

    added preview to gh-pages

---
+++ javascripts/script.js
@@ -0,0 +1,44 @@
+jQuery( function( $ ){
+
+	var facets = [{
+		type: "string",
+		name: "simple",
+		label: "Simple",
+		options: [ "frist", "second", "last" ]
+	},{
+		type: "array",
+		name: "multi",
+		label: "Array",
+		options: [ "pizza", "pasta", "carne" ]
+	},{
+		type: "number",
+		name: "number",
+		label: "Nummer",
+		min: 0,
+		max: 100,
+		step: 5
+	},{
+		type: "number",
+		name: "numberop",
+		label: "Nummer-OP",
+		min: 0,
+		max: 100,
+		step: 1,
+		operators: [ "!=", "==" ]
+	}]
+
+	var _iggy = new IGGY( $( "#iggytest1" ), facets );
+
+	var _query = _iggy.getQuery()
+	
+	var _printQuery = function( data, target ){
+		$( target ? target : "#iggytest1_result" ).html( JSON.stringify( data, null, "  " ) );
+	}
+	_printQuery( facets, "#iggytest1_config" );
+	_printQuery( _query.toJSON() )
+
+
+	_iggy.on( "change", function( qColl ){
+		_printQuery( qColl.toJSON() )
+	});
+})
